b3a9241bf251dfcc2b5e54b3cbbd9f68d10b8fc7
Serhiy <seregarachuk20@gmail.com>
2026-03-04 20:04:49 +0200

    Updating state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,16 @@
 import { useState } from "react"  
-// Додайте функцію, яка викликається при натисканні на <button>.
-// В цій функції змініть isEditing на true.
-// Показуйте <span className="player-name"> тільки коли isEditing є false.
-// Показуйте елемент <input> (який не потрібно налаштовувати), якщо isEditing є true.
-
-
-
-
 
 export default function Player({name,  symbol}) { 
     const [isEditing, setIsEditing] = useState(false);   
     
 function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(!isEditing  );
   }
-    let playerName = <span className="player-name" > {name} </span>; 
-    
+
+    let playerName = <span className="player-name" >{name} </span>; 
+    // let btnCaption = "Edit";
     if(isEditing) {
-      playerName  = <input type="text" required />
+      playerName  = <input type="text" required value={name} />
     } 
  
 
@@ -26,7 +19,7 @@ function handleEditClick() {
               {playerName} 
             <span className="Player-symbol">{symbol}</span> 
             </span> 
-            <button onClick={handleEditClick}> Edit </button>  
+            <button onClick={handleEditClick}> {isEditing ? "Save" : "Edit"} </button>  
          </li>  
             
 
