12045b78d3435fbae4a75c559d3ad32a585279b0
Serhiy <seregarachuk20@gmail.com>
2026-03-04 11:24:49 +0200

    add useState

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,32 @@
-export default function Player({name,  symbol}) {
-    return <li>
-            <span className="player-info"> 
-            <span className="player-name">{name}</span>  
+import { useState } from "react"  
+// Додайте функцію, яка викликається при натисканні на <button>.
+// В цій функції змініть isEditing на true.
+// Показуйте <span className="player-name"> тільки коли isEditing є false.
+// Показуйте елемент <input> (який не потрібно налаштовувати), якщо isEditing є true.
+
+
+
+
+
+export default function Player({name,  symbol}) { 
+    const [isEditing, setIsEditing] = useState(false);   
+    
+function handleEditClick() {
+    setIsEditing(true);
+  }
+    let playerName = <span className="player-name" > {name} </span>; 
+    
+    if(isEditing) {
+      playerName  = <input type="text" required />
+    } 
+ 
+
+    return <li> 
+            <span className="player"> 
+              {playerName} 
             <span className="Player-symbol">{symbol}</span> 
             </span> 
-            <button>Edit</button>  
+            <button onClick={handleEditClick}> Edit </button>  
          </li>  
             
 
